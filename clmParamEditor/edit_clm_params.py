ca01378b789197f82926300c8be1cf5adc06a889
qzzhang <qzhang@anl.gov>
2023-05-06 23:18:17 -0500

    able to read the .nc files and fill the dropdown boxes

---
+++ clmParamEditor/edit_clm_params.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) qzhang.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+An widget for editing parameters in a clm parameter file
+"""
+
+from ipywidgets import DOMWidget, widget_serialization
+from traitlets import Unicode, List, observe, HasTraits, default
+
+from ._frontend import module_name, module_version
+import netCDF4
+import numpy as np
+import os
+import getpass
+
+
+class EditCLMParamWidget(DOMWidget, HasTraits):
+    """TODO: Add docstring here
+    """
+    _model_name = Unicode('EditCLMParamModel').tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+    _view_name = Unicode('EditCLMParamView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    # widget model attributes
+    username = Unicode()
+    
+    clmnc_file = Unicode('./data/clm_params_files/clm_params_c180312.nc').tag(sync=True)
+    newclmnc_file = Unicode('new_clm_file.nc').tag(sync=True)
+    
+    # six parameters to expose for editing
+    # 0. single value parameter (double) -- allpfts = 1
+    r_mort = Unicode('0.02').tag(sync=True)
+
+    # five more traitlets
+    slatop = List([]).tag(sync=True)
+    flnr = List([]).tag(sync=True)
+    frootcn = List([]).tag(sync=True)
+    froot_leaf = List([]).tag(sync=True)
+    leafcn = List([]).tag(sync=True)
+
+    # To calculate a default value dynamically, decorate a method of your class with @default({traitname}).
+    @default('username')
+    def _username_default(self):
+        return getpass.getuser()
+    
+    # @observe('clmnc_file')
+    def _on_clmnc_file_changed(self, change):
+        print(f"{change.name} changed from {change.old} to {change.new}")
+        self.read_netCDF_data(change.new)
+
+    # To do something when a trait attribute is changed, decorate a method with traitlets.observe().
+    # @observe('r_mort')
+    def _on_r_mort_changed(self, change):
+        """
+        This callback is passed the following dictionary when called:
+        {
+          'owner': object,  # The HasTraits instance
+          'new': 0.03,         # The new value
+          'old': 0.02,         # The old value
+          'name': "r_mort",    # The name of the changed trait
+          'type': 'change', # The event type of the notification, usually 'change'
+        }
+        """
+        self.send({'event': 'r_mort_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+    # @observe('slatop')
+    def _on_slatop_changed(self, change):
+        self.send({'event': 'slatop_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+    # @observe('flnr')
+    def _on_flnr_changed(self, change):
+        self.send({'event': 'flnr_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+    # @observe('frootcn')
+    def _on_frootcn_changed(self, change):
+        self.send({'event': 'frootcn_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+    # @observe('froot_leaf')
+    def _on_froot_leaf_changed(self, change):
+        self.send({'event': 'froot_leaf_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+    # @observe('leafcn')
+    def _on_leafcn_changed(self, change):
+        self.send({'event': 'leafcn_changed', 'new_value': change.new})
+        print(f"{change.name} changed from {change.old} to {change.new}")
+
+
+    def __init__(self, **kwargs):
+        print("initializing the widget...")
+        super().__init__(**kwargs)
+        self.read_netCDF_data("./data/clm_params_files/clm_params_c180312.nc")
+
+        """
+        These _on_*_changed functions get called whenever the value of the data traitlet changes.
+        This method sends a message to the frontend using the self.send method, with a custom event name,
+        e.g., 'slatop_changed' and the new value of the slatop traitlet.
+        """
+        self.observe(self._on_clmnc_file_changed, names='clmnc_file')
+        self.observe(self._on_r_mort_changed, names='r_mort')
+        self.observe(self._on_slatop_changed, names='slatop')
+        self.observe(self._on_flnr_changed, names='flnr')
+        self.observe(self._on_frootcn_changed, names='frootcn')
+        self.observe(self._on_froot_leaf_changed, names='froot_leaf')
+        self.observe(self._on_leafcn_changed, names='leafcn')
+
+    def read_netCDF_data(self, clmncfile):
+        if clmncfile and os.path.isfile(clmncfile):
+            print(f'clm file to be read: {clmncfile}')
+            with netCDF4.Dataset(clmncfile) as clm_dataset:
+                # print(clm_dataset.variables['r_mort'][:][0])
+                self.r_mort = str(clm_dataset.variables['r_mort'][:][0])
+                # print(clm_dataset.variables['slatop'][:])
+                # EditCLMParamWidget.slatop = clm_dataset.variables['slatop'][:]
+                self.slatop = clm_dataset.variables['slatop'][:].tolist()
+                # print(clm_dataset.variables['flnr'][:])
+                self.flnr = clm_dataset.variables['flnr'][:].tolist()
+                # print(clm_dataset.variables['frootcn'][:])
+                self.frootcn = clm_dataset.variables['frootcn'][:].tolist()
+                # print(clm_dataset.variables['froot_leaf'][:])
+                self.froot_leaf = clm_dataset.variables['froot_leaf'][:].tolist()
+                print(clm_dataset.variables['leafcn'][:])
+                self.leafcn = clm_dataset.variables['leafcn'][:].tolist()
+        else:
+            print(f'clm file: {clmncfile} does not exist!')
+            print(f'clm file is of type {type(clmncfile)}!')
---
+++ src/widget.ts
@@ -0,0 +1,415 @@
+// Copyright (c) qzhang
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  DOMWidgetModel,
+  DOMWidgetView,
+  //WidgetModel,
+  //WidgetView,
+  ISerializers,
+} from '@jupyter-widgets/base';
+// import { List } from '@jupyter-widgets/base';
+// import * as ipywidgets from "@jupyter-widgets/base";
+
+import { MODULE_NAME, MODULE_VERSION } from './version';
+
+// Import the CSS
+import '../css/widget.css';
+
+export class EditCLMParamModel extends DOMWidgetModel {
+  defaults() {
+    return {
+      ...super.defaults(),
+      _model_name: EditCLMParamModel.model_name,
+      _model_module: EditCLMParamModel.model_module,
+      _model_module_version: EditCLMParamModel.model_module_version,
+      _view_name: EditCLMParamModel.view_name,
+      _view_module: EditCLMParamModel.view_module,
+      _view_module_version: EditCLMParamModel.view_module_version,
+
+      pftname: [
+          "not_vegetated                           ",
+          "needleleaf_evergreen_temperate_tree     ",
+          "needleleaf_evergreen_boreal_tree        ",
+          "needleleaf_deciduous_boreal_tree        ",
+          "broadleaf_evergreen_tropical_tree       ",
+          "broadleaf_evergreen_temperate_tree      ",
+          "broadleaf_deciduous_tropical_tree       ",
+          "broadleaf_deciduous_temperate_tree      ",
+          "broadleaf_deciduous_boreal_tree         ",
+          "broadleaf_evergreen_shrub               ",
+          "broadleaf_deciduous_temperate_shrub     ",
+          "broadleaf_deciduous_boreal_shrub        ",
+          "c3_arctic_grass                         ",
+          "c3_non-arctic_grass                     ",
+          "c4_grass                                ",
+          "c3_crop                                 ",
+          "c3_irrigated                            ",
+          "corn                                    ",
+          "irrigated_corn                          ",
+          "spring_temperate_cereal                 ",
+          "irrigated_spring_temperate_cereal       ",
+          "winter_temperate_cereal                 ",
+          "irrigated_winter_temperate_cereal       ",
+          "soybean                                 ",
+          "irrigated_soybean                       "] as string[],
+    };
+  }
+
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+    // Add any extra serializers here
+    /*
+    slatop: { deserialize: (value: any) => value },  // List.serializers,
+    flnr: { deserialize: (value: any) => value },  //List.serializers,
+    frootcn: { deserialize: (value: any) => value }, // List.serializers,
+    froot_leaf: { deserialize: (value: any) => value }, // List.serializers,
+    leafcn: { deserialize: (value: any) => value }  // List.serializers
+    */
+  };
+
+  static model_name = 'EditCLMParamModel';
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+  static view_name = 'EditCLMParamView'; // Set to null if no view
+  static view_module = MODULE_NAME; // Set to null if no view
+  static view_module_version = MODULE_VERSION;
+
+  initialize(attributes: any, options: any) {
+    super.initialize(attributes, options);
+    this.on('msg:custom', this.handleCustomMessage, this);
+  }
+
+  handleCustomMessage(msg: any) {
+    alert("Received a msg:" + msg);
+    if (msg.content.event === 'slatop_changed') {
+        const newData = msg.content.new_value;
+        this.set('slatop', newData);
+    }
+    if (msg.content.event === 'flnr_changed') {
+        const newData = msg.content.new_value;
+        this.set('flnr', newData);
+    }
+    if (msg.content.event === 'frootcn_changed') {
+        const newData = msg.content.new_value;
+        this.set('frootcn', newData);
+    }
+    if (msg.content.event === 'froot_leaf_changed') {
+        const newData = msg.content.new_value;
+        this.set('froot_leaf', newData);
+    }
+    if (msg.content.event === 'leafcn_changed') {
+        const newData = msg.content.new_value;
+        this.set('leafcn', newData);
+    }
+    if (msg.content.event.includes('_changed')) {
+        this.save_changes();
+    }
+  }
+}
+
+export class EditCLMParamView extends DOMWidgetView {
+  private _div0: HTMLDivElement;
+  private _div1: HTMLDivElement;
+  private _div2: HTMLDivElement;
+  private _div3: HTMLDivElement;
+
+  private _clm_filelbl: HTMLInputElement;
+  private _clm_original_file: HTMLInputElement;
+  private _clm_newlbl: HTMLInputElement;
+  private _clm_new_file: HTMLInputElement;
+
+  private _pftnamelbl: HTMLInputElement;
+  private _pftname: HTMLSelectElement;
+  private _slatop: HTMLSelectElement;
+  private _flnr: HTMLSelectElement;  
+  private _frootcn: HTMLSelectElement;
+  private _froot_leaf: HTMLSelectElement;
+  private _leafcn: HTMLSelectElement;
+  private _rmortlbl: HTMLInputElement;
+  private _r_mort: HTMLInputElement;
+
+  render() {
+    this._div0 = document.createElement('div');
+    this._div0.classList.add('widget-container', 'widget-box');
+    this._div1 = document.createElement('div');
+    this._div1.classList.add('widget-container', 'widget-box');
+    this._div2 = document.createElement('div');
+    this._div2.classList.add('widget-container', 'widget-box');
+    this._div3 = document.createElement('div');
+    this._div3.classList.add('widget-container', 'widget-box');
+
+    this._clm_filelbl = document.createElement('input');
+    this._clm_filelbl.type = 'label';
+    this._clm_filelbl.value = 'clm_parameters_file: ';
+    this._clm_filelbl.disabled = true;
+    this._clm_filelbl.classList.add('widget-glabel');
+    this._div0.appendChild(this._clm_filelbl);
+
+    this._clm_original_file = document.createElement('input');
+    this._clm_original_file.type = 'text';
+    this._clm_original_file.value = this.model.get('clmnc_file');
+    this._clm_original_file.disabled = false;
+    this._clm_original_file.classList.add('widget-input');
+    this._div0.appendChild(this._clm_original_file);
+
+    this._clm_newlbl = document.createElement('input');
+    this._clm_newlbl.type = 'label';
+    this._clm_newlbl.value = 'Save to new file: ';
+    this._clm_newlbl.disabled = true;
+    this._clm_newlbl.classList.add('widget-glabel');
+    this._div0.appendChild(this._clm_newlbl);
+
+    this._clm_new_file = document.createElement('input');
+    this._clm_new_file.type = 'text';
+    this._clm_new_file.value = this.model.get('newclmnc_file');  //'new_' + this.model.get('clmnc_file');
+    this._clm_new_file.disabled = false;
+    this._clm_new_file.classList.add('widget-input');
+    this._div0.appendChild(this._clm_new_file);
+
+    // Create select element
+
+    this._pftnamelbl = document.createElement('input');
+    this._pftnamelbl.type = 'label';
+    this._pftnamelbl.value = 'plant function type: ';
+    this._pftnamelbl.disabled = true;
+    this._pftnamelbl.classList.add('widget-blabel');
+    this._div1.appendChild(this._pftnamelbl);
+
+    const list = this.model.get('pftname');
+    console.log(list);
+    this._pftname = document.createElement('select'); 
+    this._create_dropdown1(this.model.get('pftname'));
+    this._pftname.selectedIndex = 0;
+    this._pftname.disabled = false;
+    this._pftname.classList.add('widget-pftname');
+    this._div1.appendChild(this._pftname);
+
+    this._rmortlbl = document.createElement('input');
+    this._rmortlbl.type = 'label';
+    this._rmortlbl.value = 'Mortality rate: ';
+    this._rmortlbl.disabled = true;
+    this._rmortlbl.classList.add('widget-blabel');
+    this._div1.appendChild(this._rmortlbl);
+
+    this._r_mort = document.createElement('input');
+    this._r_mort.type = 'text';
+    this._r_mort.value = this.model.get('r_mort');
+    this._r_mort.disabled = false;
+    this._r_mort.classList.add('widget-num');
+    this._div1.appendChild(this._r_mort);
+
+    /* 6 variables: slatop; flnr; frootcn; froot_leaf; leafcn; r_mort*/
+    const vars = ['Canopy SLA ', 'Rubisco N allocation', 'Fine root C:N', 'NewRoot C/NewLeaf C', 'Leaf C:N'];
+    for (const ind1 in vars) {
+        // create the table headers
+        const lblelm: HTMLInputElement = document.createElement('input');
+        lblelm.type = 'label';
+        lblelm.value = vars[ind1];
+        lblelm.disabled = true;
+        lblelm.classList.add('widget-blabel');
+        this._div2.appendChild(lblelm);
+    }
+    this._slatop = document.createElement('select');
+    this._flnr = document.createElement('select');
+    this._frootcn = document.createElement('select');
+    this._froot_leaf = document.createElement('select');
+    this._leafcn = document.createElement('select');
+    this._create_dropdown2(this.model.get('slatop'), this._slatop);
+    this._slatop.classList.add('widget-number');
+    this._div3.appendChild(this._slatop);
+    this._create_dropdown2(this.model.get('flnr'), this._flnr);
+    this._flnr.classList.add('widget-number');
+    this._div3.appendChild(this._flnr);
+    this._create_dropdown2(this.model.get('frootcn'), this._frootcn);
+    this._frootcn.classList.add('widget-number');
+    this._div3.appendChild(this._frootcn);
+    this._create_dropdown2(this.model.get('froot_leaf'), this._froot_leaf);
+    this._froot_leaf.classList.add('widget-number');
+    this._div3.appendChild(this._froot_leaf);
+    this._create_dropdown2(this.model.get('leafcn'), this._leafcn);
+    this._leafcn.classList.add('widget-number');
+    this._div3.appendChild(this._leafcn);
+
+    // Attach DOM to el
+    this.el.appendChild(this._div0);
+    this.el.appendChild(this._div1);
+    this.el.appendChild(this._div2);
+    this.el.appendChild(this._div3);
+    this.el.classList.add('custom-widget');
+
+    
+    // Python -> JavaScript update
+    // Monitoring the changes in the widget's model (front end)
+    this.model.on('change:clmnc_file', this._onOriginalFileChanged, this);
+    this.model.on('change:newclmnc_file', this._onNewFileChanged, this);
+    this.model.on('change:slatop', this._onSlatopChanged, this);
+    this.model.on('change:flnr', this._onFlnrChanged, this);
+    this.model.on('change:frootcn', this._onFrootCNChanged, this);
+    this.model.on('change:froot_leaf', this._onFrootLeafChanged, this);
+    this.model.on('change:leafcn', this._onLeafCNChanged, this);
+    this.model.on('change:r_mort', this._onRmortChanged, this);
+
+    // JavaScript -> Python update
+    // Listening to changes of the widget's own DOM attribute changes
+    // this.listenTo(this.model, 'change:slatop', this._onSlatopInputChanged, this);
+    this._clm_original_file.onchange = this._onOriginalFileInputChanged.bind(this);
+    this._clm_new_file.onchange = this._onNewFileInputChanged.bind(this);
+    this._pftname.onchange = this._onPftnameInputChanged.bind(this);
+    this._slatop.onchange = this._onSlatopInputChanged.bind(this);
+    this._flnr.onchange = this._onFlnrInputChanged.bind(this);
+    this._frootcn.onchange = this._onFrootCNInputChanged.bind(this);
+    this._froot_leaf.onchange = this._onFrootLeafInputChanged.bind(this);
+    this._leafcn.onchange = this._onLeafCNInputChanged.bind(this);
+    this._r_mort.onchange = this._onRmortInputChanged.bind(this);
+
+  }
+
+  /**** get/set and other helper methods for render()****/
+
+  // get (Python -> JavaScript update)
+  private _onOriginalFileChanged() {
+    this._clm_original_file.value = this.model.get('clmnc_file');
+  }
+  private _onNewFileChanged() {
+    this._clm_new_file.value = this.model.get('newclmnc_file');
+  }
+
+  private _onSlatopChanged() {
+    const newSlatop: number[] = this.model.get('slatop');
+    this._create_dropdown2(newSlatop, this._slatop);
+    this.send({ event: 'slatop_changed', new_value: newSlatop });
+  }
+  private _onFlnrChanged() {
+    const newFlnr: number[] = this.model.get('flnr');
+    this._create_dropdown2(newFlnr, this._flnr);
+    this.send({ event: 'flnr_changed', new_value: newFlnr });
+  }
+  private _onFrootCNChanged() {
+    const newFrootcn: number[] = this.model.get('frootcn');
+    this._create_dropdown2(newFrootcn, this._frootcn);
+    this.send({ event: 'frootcn_changed', new_value: newFrootcn });
+  }
+  private _onFrootLeafChanged() {
+    const newFrootLeaf: number[] = this.model.get('froot_leaf');
+    this._create_dropdown2(newFrootLeaf, this._froot_leaf);
+    this.send({ event: 'froot_leaf_changed', new_value: newFrootLeaf });
+  }
+  private _onLeafCNChanged() {
+    const newLeafcn: number[] = this.model.get('leafcn');
+    alert('leafcn' + ' is changed to new value: ' + newLeafcn)
+    this._create_dropdown2(newLeafcn, this._leafcn);
+    this.send({ event: 'leafcn_changed', new_value: newLeafcn });
+  }
+  private _onRmortChanged() {
+    alert('r_mort' + ' is changed to new value: ' + this.model.get('r_mort'))
+    this._r_mort.value = this.model.get('r_mort');
+  }
+
+  // Define a function to create editable options
+  private _createEditableOption(value: number, text: number) {
+    const optionElement = document.createElement("option");
+    optionElement.value = value.toString();
+    optionElement.text = text.toString();
+
+    // Add an event listener to allow editing of the option text
+    optionElement.addEventListener("dblclick", () => {
+      const newOptionText = prompt("Enter new value:", value.toString());
+      if (newOptionText !== null) {
+        optionElement.text = newOptionText;
+      }
+    });
+    return optionElement;
+  }
+
+  private _create_dropdown1(items: string[]) {
+    console.log(items);
+    for (const index in items) {
+      const optionElement = document.createElement("option");
+      optionElement.value = index;
+      optionElement.text = items[index];
+      this._pftname.appendChild(optionElement);
+    }
+  }
+  private _create_dropdown2(items: number[], dom_sel: HTMLSelectElement) {
+    for (const index in items) {
+      const optionElement = this._createEditableOption(+index, items[index]);
+      dom_sel.appendChild(optionElement);
+    }
+    //dom_sel.selectedIndex = 0;
+    dom_sel.disabled = false;
+    dom_sel.size = 25;
+    dom_sel.addEventListener("scroll", function(event) {
+        event.preventDefault();
+        return false;
+    });
+    dom_sel.addEventListener("mousedown", function(event) {
+        event.preventDefault();
+        return false;
+    });
+    dom_sel.addEventListener("keydown", function(event) {
+        event.preventDefault();
+        return false;
+    });
+    //dom_sel.classList.add('widget-select');
+  }
+
+  // set (JavaScript -> Python update)
+  // get the values updated from the front-end to the Python kernel
+  private _onOriginalFileInputChanged() {
+    this.model.set('clmnc_file', this._clm_original_file.value);
+    this.model.save_changes();
+  }
+  private _onNewFileInputChanged() {
+    this.model.set('newclmnc_file', this._clm_new_file.value);
+    this.model.save_changes();      
+  }
+  private _onPftnameInputChanged() {
+    const selectedInd = this._pftname.selectedIndex;
+    // this._pftname.value = this._pftname.options[selectedInd].value;
+    this._slatop.selectedIndex = selectedInd;
+    this._flnr.selectedIndex = selectedInd;
+    this._frootcn.selectedIndex = selectedInd;
+    this._froot_leaf.selectedIndex = selectedInd;
+    this._leafcn.selectedIndex = selectedInd;
+  }
+  private _onSlatopInputChanged() {
+    this.model.set('slatop', this.options_to_array(this._slatop.options));
+    this.model.save_changes();
+  }
+  private _onFlnrInputChanged() {
+    this.model.set('flnr', this.options_to_array(this._flnr.options));
+    this.model.save_changes();
+  }
+  private _onFrootCNInputChanged() {
+    this.model.set('frootcn', this.options_to_array(this._frootcn.options));
+    this.model.save_changes();
+  }
+  private _onFrootLeafInputChanged() {
+    this.model.set('froot_leaf', this.options_to_array(this._froot_leaf.options));
+    this.model.save_changes();
+  }
+  private _onLeafCNInputChanged() {
+    this.model.set('leafcn', this.options_to_array(this._leafcn.options));
+    console.log(this.model.get('leafcn'));
+    this.model.save_changes();
+  }
+  private _onRmortInputChanged() {
+    this.model.set('r_mort', this._r_mort.value);
+    this.model.save_changes();
+  }
+
+  private options_to_array(options: HTMLOptionsCollection) {
+    let arr_ret: number[] = [];
+
+    if(options.length >= 1) {
+        for (const index in options) {
+            if (options[index] !== null && options[index].text !== null) {
+                arr_ret.push(+options[index].text);
+            }
+        }
+    }
+    return arr_ret;
+  }
+
+}
